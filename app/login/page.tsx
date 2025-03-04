f9165a50895cd73d8a229cebfb67b15dc72559df
Keegan Botha <keegan@owlbox.com>
2025-03-04 12:26:27 +0200

    Setting up auth for the proj

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,9 @@
+import { Form } from "./form";
+
 export default function Page() {
-  return <div>This is the login</div>;
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <Form />
+    </div>
+  );
 }
