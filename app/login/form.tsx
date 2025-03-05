d52ff5e2486469a78ad583f7dc9d8cb56da1cd25
Keegan Botha <keegan@owlbox.com>
2025-03-05 12:43:24 +0200

    implemented prisma and created the local db

--- app/login/form.tsx
+++ app/login/form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -6,11 +8,13 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
-import { Label } from "@radix-ui/react-label";
+import { signIn } from "next-auth/react";
 
 export function Form() {
+  function handleGithubLogin() {
+    signIn("github");
+  }
   return (
     <div className={cn("flex flex-col gap-6")}>
       <Card>
@@ -23,38 +27,26 @@ export function Form() {
         <CardContent>
           <form>
             <div className="grid gap-6">
-              <div className="grid gap-6">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <a
-                      href="#"
-                      className="ml-auto text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </a>
-                  </div>
-                  <Input id="password" type="password" required />
-                </div>
-                <Button type="submit" className="w-full">
-                  Login
+              <div className="flex flex-col gap-4">
+                <Button
+                  onClick={handleGithubLogin}
+                  variant="outline"
+                  className="w-full"
+                >
+                  <svg
+                    height="32"
+                    aria-hidden="true"
+                    viewBox="0 0 24 24"
+                    version="1.1"
+                    width="32"
+                    data-view-component="true"
+                    className="octicon octicon-mark-github v-align-middle"
+                  >
+                    <path d="M12.5.75C6.146.75 1 5.896 1 12.25c0 5.089 3.292 9.387 7.863 10.91.575.101.79-.244.79-.546 0-.273-.014-1.178-.014-2.142-2.889.532-3.636-.704-3.866-1.35-.13-.331-.69-1.352-1.18-1.625-.402-.216-.977-.748-.014-.762.906-.014 1.553.834 1.769 1.179 1.035 1.74 2.688 1.25 3.349.948.1-.747.402-1.25.733-1.538-2.559-.287-5.232-1.279-5.232-5.678 0-1.25.445-2.285 1.178-3.09-.115-.288-.517-1.467.115-3.048 0 0 .963-.302 3.163 1.179.92-.259 1.897-.388 2.875-.388.977 0 1.955.13 2.875.388 2.2-1.495 3.162-1.179 3.162-1.179.633 1.581.23 2.76.115 3.048.733.805 1.179 1.825 1.179 3.09 0 4.413-2.688 5.39-5.247 5.678.417.36.776 1.05.776 2.128 0 1.538-.014 2.774-.014 3.162 0 .302.216.662.79.547C20.709 21.637 24 17.324 24 12.25 24 5.896 18.854.75 12.5.75Z"></path>
+                  </svg>
+                  Login with Github
                 </Button>
               </div>
-              {/* <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <a href="#" className="underline underline-offset-4">
-                  Sign up
-                </a>
-              </div> */}
             </div>
           </form>
         </CardContent>
