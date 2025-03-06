1a8281561edfc465b6dbf8a29cddabb4992e27d6
Keegan Botha <keegan@owlbox.com>
2025-03-06 07:02:08 +0200

    Got github OAuth Working

--- app/login/form.tsx
+++ app/login/form.tsx
@@ -9,30 +9,30 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import { signIn } from "next-auth/react";
+import { login } from "./action";
 
 export function Form() {
-  function handleGithubLogin() {
-    signIn("github");
-  }
+  const handleGithubLogin = async () => {
+    await login();
+  };
+
   return (
     <div className={cn("flex flex-col gap-6")}>
       <Card>
         <CardHeader>
           <CardTitle>Login</CardTitle>
-          <CardDescription>
-            Login with your Apple or Google account
-          </CardDescription>
+          <CardDescription>Login with your GitHub account</CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleGithubLogin();
+            }}
+          >
             <div className="grid gap-6">
               <div className="flex flex-col gap-4">
-                <Button
-                  onClick={handleGithubLogin}
-                  variant="outline"
-                  className="w-full"
-                >
+                <Button type="submit" variant="outline" className="w-full">
                   <svg
                     height="32"
                     aria-hidden="true"
@@ -40,7 +40,7 @@ export function Form() {
                     version="1.1"
                     width="32"
                     data-view-component="true"
-                    className="octicon octicon-mark-github v-align-middle"
+                    className="octicon octicon-mark-github v-align-middle mr-2"
                   >
                     <path d="M12.5.75C6.146.75 1 5.896 1 12.25c0 5.089 3.292 9.387 7.863 10.91.575.101.79-.244.79-.546 0-.273-.014-1.178-.014-2.142-2.889.532-3.636-.704-3.866-1.35-.13-.331-.69-1.352-1.18-1.625-.402-.216-.977-.748-.014-.762.906-.014 1.553.834 1.769 1.179 1.035 1.74 2.688 1.25 3.349.948.1-.747.402-1.25.733-1.538-2.559-.287-5.232-1.279-5.232-5.678 0-1.25.445-2.285 1.178-3.09-.115-.288-.517-1.467.115-3.048 0 0 .963-.302 3.163 1.179.92-.259 1.897-.388 2.875-.388.977 0 1.955.13 2.875.388 2.2-1.495 3.162-1.179 3.162-1.179.633 1.581.23 2.76.115 3.048.733.805 1.179 1.825 1.179 3.09 0 4.413-2.688 5.39-5.247 5.678.417.36.776 1.05.776 2.128 0 1.538-.014 2.774-.014 3.162 0 .302.216.662.79.547C20.709 21.637 24 17.324 24 12.25 24 5.896 18.854.75 12.5.75Z"></path>
                   </svg>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,51 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Label } from "@radix-ui/react-label";
 import { Input } from "@/components/ui/input";
+import { Form } from "./form";
 
 export default function Page() {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      <form>
-        <div className="grid gap-6">
-          <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-            <span className="relative z-10 bg-background px-2 text-muted-foreground">
-              Or continue with
-            </span>
-          </div>
-          <div className="grid gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <a
-                  href="#"
-                  className="ml-auto text-sm underline-offset-4 hover:underline"
-                >
-                  Forgot your password?
-                </a>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-          </div>
-          <div className="text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <a href="#" className="underline underline-offset-4">
-              Sign up
-            </a>
-          </div>
-        </div>
-      </form>
+      <Form />;
     </div>
   );
 }
