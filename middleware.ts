d78a0605ca2bc1bb9e52bd59958311e46341f010
Keegan Botha <keegan@owlbox.com>
2025-03-06 12:53:20 +0200

    Implemented middle ware to kick the user when they are not logged in

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,24 @@
-export { auth as middleware } from "@/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+import { auth } from "@/auth";
+
+const PUBLIC_ROUTES = ["/login"];
+
+export async function middleware(req: NextRequest) {
+  const session = await auth();
+
+  const { nextUrl } = req;
+  const currLocation = nextUrl?.pathname;
+  const isAuthenticated = !!session;
+  const isPublicRoute = PUBLIC_ROUTES.includes(currLocation);
+
+  if (!isAuthenticated && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
