aab1fd2837a4175e96b28697f9717c748e661ff9
Keegan Botha <keegan@owlbox.com>
2025-03-06 07:03:07 +0200

    Cleaned up some mistakes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { Label } from "@radix-ui/react-label";
-import { Input } from "@/components/ui/input";
 import { Form } from "./form";
 
 export default function Page() {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      <Form />;
+      <Form />
     </div>
   );
 }
