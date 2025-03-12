0d43a1dc3ab684f0b322ee2aef2a571644f25729
Keegan Botha <keegan@owlbox.com>
2025-03-12 19:19:48 +0200

    Set up the H tag styles and fixed my understanding of prisma generation

--- components/app-bar/Title.tsx
+++ components/app-bar/Title.tsx
@@ -6,5 +6,5 @@ export function Title() {
   const pathname = usePathname();
   const title = pathname.split("/").pop();
 
-  return <h4>{title}</h4>;
+  return <h4 className="capitalize">{title}</h4>;
 }
