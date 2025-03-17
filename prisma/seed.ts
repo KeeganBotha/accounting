b26711854d94fc40a5156e4f7ff27223da603db5
Keegan Botha <keegan@owlbox.com>
2025-03-17 12:55:51 +0200

    Working on the seeding file

---
+++ prisma/seed.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const alice = await prisma.sideMenu.create({
+    data: {
+      name: "Home",
+    },
+  });
+  const bob = await prisma.user.upsert({
+    where: { email: "bob@prisma.io" },
+    update: {},
+    create: {
+      email: "bob@prisma.io",
+      name: "Bob",
+      posts: {
+        create: [
+          {
+            title: "Follow Prisma on Twitter",
+            content: "https://twitter.com/prisma",
+            published: true,
+          },
+          {
+            title: "Follow Nexus on Twitter",
+            content: "https://twitter.com/nexusgql",
+            published: true,
+          },
+        ],
+      },
+    },
+  });
+  console.log({ alice, bob });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
