4f31fc89f1fa5b1a9b92a8c7045fe8fd7b3438da
Keegan Botha <keegan@owlbox.com>
2025-03-18 12:06:03 +0200

    Got the side menu to seed, great success

--- prisma/migrations/20250318050428_/migration.sql
+++ prisma/migrations/20250318050428_/migration.sql
@@ -1,7 +1,18 @@
 -- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "side_menu" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
+    "iconName" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
     "parentId" INTEGER,
 
     CONSTRAINT "side_menu_pkey" PRIMARY KEY ("id")
@@ -15,6 +26,9 @@ CREATE TABLE "side_menu_settings" (
 
     CONSTRAINT "side_menu_settings_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
 
 -- AddForeignKey
 ALTER TABLE "side_menu" ADD CONSTRAINT "side_menu_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "side_menu"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,34 +2,20 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const alice = await prisma.sideMenu.create({
-    data: {
-      name: "Home",
-    },
-  });
-  const bob = await prisma.user.upsert({
-    where: { email: "bob@prisma.io" },
-    update: {},
-    create: {
-      email: "bob@prisma.io",
-      name: "Bob",
-      posts: {
-        create: [
-          {
-            title: "Follow Prisma on Twitter",
-            content: "https://twitter.com/prisma",
-            published: true,
-          },
-          {
-            title: "Follow Nexus on Twitter",
-            content: "https://twitter.com/nexusgql",
-            published: true,
-          },
-        ],
+  const sideMenu = await prisma.sideMenu.createMany({
+    data: [
+      {
+        name: "Home",
+        path: "/private/home",
+        iconName: "",
       },
-    },
+      {
+        name: "Ledgers",
+        path: "/private/ledgers",
+        iconName: "",
+      },
+    ],
   });
-  console.log({ alice, bob });
 }
 main()
   .then(async () => {
