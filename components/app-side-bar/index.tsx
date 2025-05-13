d2d13dd0517be224c140350923ce03b5a7a6a14c
Keegan Botha <keegan@owlbox.com>
2025-05-13 07:02:23 +0200

    implementing the menu

--- components/app-side-bar/index.tsx
+++ components/app-side-bar/index.tsx
@@ -4,12 +4,15 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
-  SidebarGroupContent,
   SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
 } from "@/components/ui/sidebar";
 import { InferSafeActionFnResult } from "next-safe-action";
 import {
@@ -17,6 +20,8 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "../ui/collapsible";
+import { Icon } from "../icon";
+import { NavUser } from "./footer";
 
 type AppSidebarProps = {
   items: NonNullable<InferSafeActionFnResult<typeof getSideMenu>["data"]>;
@@ -24,33 +29,66 @@ type AppSidebarProps = {
 
 export function AppSidebar({ items }: AppSidebarProps) {
   return (
-    <Sidebar>
-      <SidebarHeader>Test</SidebarHeader>
+    <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <div>Header</div>
+      </SidebarHeader>
       <SidebarContent>
-        <Collapsible defaultOpen className="group/collapsible">
-          <SidebarGroup>
-            <SidebarGroupLabel>
-              <CollapsibleTrigger>Navigation</CollapsibleTrigger>
-            </SidebarGroupLabel>
-            <CollapsibleContent>
-              <SidebarGroupContent>
-                <SidebarMenu>
-                  {items.map((item) => (
-                    <SidebarMenuItem key={item.id}>
-                      <SidebarMenuButton asChild>
-                        <a href={item.path}>
-                          <span>{item.name}</span>
-                        </a>
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  ))}
-                </SidebarMenu>
-              </SidebarGroupContent>
-            </CollapsibleContent>
-          </SidebarGroup>
-        </Collapsible>
+        <MainNav items={items} />
+        {/* <NavProjects projects={data.projects} /> */}
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <NavUser user={{ avatar: "", email: "", name: "" }} />
+      </SidebarFooter>
+      <SidebarRail />
     </Sidebar>
+  );
+}
+
+type MainNavProps = {
+  items: NonNullable<InferSafeActionFnResult<typeof getSideMenu>["data"]>;
+};
+
+function MainNav({ items }: MainNavProps) {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Platform</SidebarGroupLabel>
+      <SidebarMenu>
+        {items.map((item) => (
+          <Collapsible
+            key={item.id}
+            asChild
+            defaultOpen={false}
+            className="group/collapsible"
+          >
+            <SidebarMenuItem>
+              <CollapsibleTrigger asChild>
+                <SidebarMenuButton tooltip={item.name}>
+                  <Icon iconName={item.iconName as any} />
+                  <span>{item.name}</span>
+                  <Icon
+                    iconName="chevronDown"
+                    className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90"
+                  />
+                </SidebarMenuButton>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <SidebarMenuSub>
+                  {item.children?.map((subItem) => (
+                    <SidebarMenuSubItem key={subItem.name}>
+                      <SidebarMenuSubButton asChild>
+                        <a href={subItem.path}>
+                          <span>{subItem.name}</span>
+                        </a>
+                      </SidebarMenuSubButton>
+                    </SidebarMenuSubItem>
+                  ))}
+                </SidebarMenuSub>
+              </CollapsibleContent>
+            </SidebarMenuItem>
+          </Collapsible>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
   );
 }
