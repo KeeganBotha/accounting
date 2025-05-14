7257cd5e368ec3276c49b16f6ef7dc55136f012c
Keegan Botha <keegan@owlbox.com>
2025-05-14 12:46:21 +0200

    Added some extra scripts

--- prisma/migrations/20250514104553_init/migration.sql
+++ prisma/migrations/20250514104553_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "user" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
+    "lastLogin" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
@@ -18,23 +18,8 @@ CREATE TABLE "side_menu" (
     CONSTRAINT "side_menu_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "side_menu_settings" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "sideMenuId" INTEGER NOT NULL,
-
-    CONSTRAINT "side_menu_settings_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
 
 -- AddForeignKey
 ALTER TABLE "side_menu" ADD CONSTRAINT "side_menu_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "side_menu"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "side_menu_settings" ADD CONSTRAINT "side_menu_settings_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "side_menu_settings" ADD CONSTRAINT "side_menu_settings_sideMenuId_fkey" FOREIGN KEY ("sideMenuId") REFERENCES "side_menu"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
