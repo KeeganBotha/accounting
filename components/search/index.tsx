af6146b374cfb81670e72404ce1d3945edaa0058
Keegan Botha <keegan@owlbox.com>
2025-05-30 15:30:54 +0200

    Changes

--- components/search/index.tsx
+++ components/search/index.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import lodash from "lodash";
 import { Input } from "../ui/input";
 import { Icon } from "../icon";
+import { useSearchParamsCrud } from "@/lib/useSearchParamsCrud";
+import React from "react";
 
 export function Search() {
-  const searchParams = useSearchParams();
-  const value = searchParams.get("search") ?? "";
+  const { set, get } = useSearchParamsCrud();
+  const initialValue = get("search");
+  const [search, setSearch] = React.useState(initialValue);
+
+  const handleSearch = lodash.debounce((search: string) => {}, 1000);
 
   return (
     <div className="relative">
@@ -14,7 +19,7 @@ export function Search() {
         iconName="Search"
         className="absolute left-2 top-1/2 transform -translate-y-1/2 h-4 w-4"
       />
-      <Input placeholder="Search..." className="max-w-xs pl-8" />
+      <Input placeholder="Search..." className="max-w-xs pl-8" value={search} />
     </div>
   );
 }
