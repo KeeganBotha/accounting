393d1024c911794e6b2a5b3d46dab08a3f6765a5
Keegan Botha <keegan@owlbox.com>
2025-06-11 12:01:04 +0200

    Fetched the accounts, need to mutate some

--- app/private/finance-tracker/personal/page.tsx
+++ app/private/finance-tracker/personal/page.tsx
@@ -1,5 +1,14 @@
 import { List } from "../_components/List";
+import { getPersonalAccounts } from "./action";
 
-export default function Page() {
-  return <List accounts={[]} />;
+type PageProps = {
+  searchParams: Promise<{ search: string }>;
+};
+
+export default async function Page({ searchParams }: PageProps) {
+  const { search } = await searchParams;
+  const query = await getPersonalAccounts(search ?? "");
+  const accounts = query?.data?.result ?? [];
+
+  return <List accounts={accounts} />;
 }
