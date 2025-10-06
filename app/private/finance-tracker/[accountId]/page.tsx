390b3b967c656e591bdd0cce0dbbc806836491a3
Keegan Botha <keegan@owlbox.com>
2025-10-06 15:26:49 +0200

    deleted a lot of un-needed code

--- app/private/finance-tracker/[accountId]/page.tsx
+++ app/private/finance-tracker/[accountId]/page.tsx
@@ -1,7 +1,6 @@
 import { columns } from "./columns";
 import { getAccount } from "../personal/action";
 import { DataTable } from ".";
-import { getAccountRecordTypeOptions } from "../../settings/account-record-type-options/action";
 
 type PageProps = {
   params: Promise<{ accountId: number }>;
@@ -10,20 +9,16 @@ type PageProps = {
 export default async function Page({ params }: PageProps) {
   const { accountId } = await params;
 
-  const [query, optionsQuert] = await Promise.all([
-    getAccount(accountId),
-    getAccountRecordTypeOptions(""),
-  ]);
+  const query = await getAccount(accountId);
 
   const data = query?.data?.result ?? [];
-  const options = optionsQuert?.data?.result ?? [];
 
   return (
     <DataTable
       columns={columns}
       data={data}
       accountId={accountId}
-      options={options}
+      options={[]}
     />
   );
 }
